71cf94aa88eeffb2952d778b104156715487e521
heddy <120495158+Didy-12@users.noreply.github.com>
2023-12-30 11:32:52 +0100

    Update lesson-list-page.component.ts

--- frontend/src/app/lesson-list-page/lesson-list-page.component.ts
+++ frontend/src/app/lesson-list-page/lesson-list-page.component.ts
@@ -1,9 +1,25 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+interface Flashcard {
+  question: string;
+  answer: string;
+}
+
+interface Lesson {
+  title: string;
+  flashcards: Flashcard[];
+}
+
 @Component({
   selector: 'app-lesson-list-page',
   templateUrl: './lesson-list-page.component.html'
 })
-  export class LessonListPageComponent implements OnInit, OnDestroy {
+export class LessonListPageComponent implements OnInit, OnDestroy {
+  lessonTitle: string = '';
+  question: string = '';
+  answer: string = '';
+  lessons: Lesson[] = [];
+
   constructor() {
     console.log("LessonListPageComponent.constructor()");
   }
@@ -14,5 +30,27 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 
   ngOnDestroy(): void {
     console.log("LessonListPageComponent.ngOnDestroy()");
+  }
+
+  addLesson(): void {
+    if (this.lessonTitle) {
+      this.lessons.push({
+        title: this.lessonTitle,
+        flashcards: []
+      });
+      this.lessonTitle = '';
+    }
+  }
+
+  addFlashcard(): void {
+    if (this.question && this.answer && this.lessons.length > 0) {
+      const lastLesson = this.lessons[this.lessons.length - 1];
+      lastLesson.flashcards.push({
+        question: this.question,
+        answer: this.answer
+      });
+      this.question = '';
+      this.answer = '';
+    }
   }
 }
