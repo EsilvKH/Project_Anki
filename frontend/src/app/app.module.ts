62ce0c3a169ef9ecda950d85497e91481e6d53e9
heddy <120495158+Didy-12@users.noreply.github.com>
2023-12-30 12:48:53 +0100

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -24,6 +24,7 @@ import {HttpClientModule} from "@angular/common/http";
     LessonDetailPageComponent,
     LoginComponent,
     InscriptionComponent,
+    StudyPageComponent,
   ],
   imports: [
     BrowserModule,
